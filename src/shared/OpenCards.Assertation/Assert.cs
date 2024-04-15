9aaec42f133d10a332f3f2d403023e280f929935
sipasi <tanksasha77@gmail.com>
2024-04-15 14:12:17 +0300

    feat: add assertion utilities

---
+++ src/shared/OpenCards.Assertation/Assert.cs
@@ -0,0 +1,59 @@
+﻿namespace OpenCards.Shared.Assertation;
+
+public static class Assert
+{
+    public static void IsFalse(bool condition, string? message = null)
+    {
+        if (condition is false)
+        {
+            return;
+        }
+
+        AssertException.Throw(actual: condition, expected: false, info: message);
+    }
+    public static void IsTrue(bool condition, string? message = null)
+    {
+        if (condition is true)
+        {
+            return;
+        }
+
+        AssertException.Throw(actual: condition, expected: true, info: message);
+    }
+
+    public static void AreEqual<T>(T? actual, T? expected, string? message = null)
+    {
+        bool equal = EqualityComparer<T>.Default.Equals(actual, expected);
+
+        if (equal)
+        {
+            return;
+        }
+
+        AssertException.Throw(actual, expected, info: message);
+    }
+
+    public static void IsDefault<T>(T? actual, string? message = null)
+    {
+        bool equal = EqualityComparer<T>.Default.Equals(actual, default);
+
+        if (equal)
+        {
+            return;
+        }
+
+        AssertException.Throw(actual, default, info: message);
+    }
+
+    public static void IsNotDefault<T>(T? actual, string? message = null)
+    {
+        bool equal = EqualityComparer<T>.Default.Equals(actual, default);
+
+        if (equal == false)
+        {
+            return;
+        }
+
+        AssertException.Throw(actual?.ToString(), "not default", info: message);
+    }
+}
---
+++ src/shared/OpenCards.Assertation/AssertException.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace OpenCards.Shared.Assertation;
+
+public class AssertException : Exception
+{
+    private AssertException(string message) : base(message: message) { }
+
+    public static void Throw<T>(T actual, T expected, string? info = null)
+    {
+        string message = string.Concat("Actual was ", actual, ", but expected ", expected, Environment.NewLine, info);
+
+        throw new AssertException(message);
+    }
+}
