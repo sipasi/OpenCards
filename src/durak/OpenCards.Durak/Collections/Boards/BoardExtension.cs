95466b14160e54744244248c250684db752c8562
sipasi <tanksasha77@gmail.com>
2024-04-25 14:30:54 +0300

    feat: add BoardExtension class to OpenCards.Durak.Collections.Boards namespace
    This commit introduces the BoardExtension class to the OpenCards.Durak.Collections.Boards namespace. The BoardExtension class provides extension methods for the IReadonlyBoard<SuitRankCard> interface.
    
    The following extension methods are included:
    - ContainsSuit: Determines whether the board contains a card of the same suit as the specified card.
    - ContainsRank: Determines whether the board contains a card of the same rank as the specified card.
    - CollisionsRank: Determines whether there are any collisions (cards of the same rank) between the cards on the board and the specified collection of cards.
    
    These extension methods enhance the functionality of board objects by enabling convenient checks for card suits and ranks.

---
+++ src/durak/OpenCards.Durak/Collections/Boards/BoardExtension.cs
@@ -0,0 +1,11 @@
+﻿using OpenCards.Cards.SuitsRanks;
+using OpenCards.Collections.Boards;
+
+namespace OpenCards.Durak.Collections.Boards;
+
+public static class BoardExtension
+{
+    public static bool ContainsSuit(this IReadonlyBoard<SuitRankCard> board, SuitRankCard card) => board.Any(card.EqualSuit);
+    public static bool ContainsRank(this IReadonlyBoard<SuitRankCard> board, SuitRankCard card) => board.Any(card.EqualRank);
+    public static bool CollisionsRank(this IReadonlyBoard<SuitRankCard> board, IEnumerable<SuitRankCard> cards) => board.Any(item => cards.Any(item.EqualRank));
+}
