e1f06ccf9e9254168ee6de48f10d6815e1b5770e
sipasi <tanksasha77@gmail.com>
2024-04-26 13:31:04 +0300

    feat: add MovementArguments Record Struct
    - Added a readonly record struct named MovementArguments to represent arguments for a movement action.
    - Includes properties for the current player, the deck, the board, and the type of player action.
    - Provides a convenient immutable way to pass movement-related arguments.

---
+++ src/durak/OpenCards.Durak/Movements/MovementArguments.cs
@@ -0,0 +1,12 @@
+﻿using OpenCards.Cards.SuitsRanks;
+using OpenCards.Collections.Boards;
+using OpenCards.Durak.Collections.Decks;
+using OpenCards.Durak.Players;
+
+namespace OpenCards.Durak.Movements;
+
+public readonly record struct MovementArguments(
+    IReadonlyPlayer Current,
+    IReadonlyDurakDeck Deck,
+    IReadonlyBoard<SuitRankCard> Board,
+    PlayerActionType ActionType);
