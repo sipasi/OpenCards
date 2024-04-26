eebcb270a8b0cab8df34cf1c6b23a3de7bb7bd07
sipasi <tanksasha77@gmail.com>
2024-04-26 13:32:26 +0300

    feat: add IPlayerMovement Interface
    - Introduced the IPlayerMovement interface within the OpenCards.Durak.Movements namespace.
    - Defines a contract for implementing player movements in the Durak game.
    - Includes a method signature for making a player movement asynchronously, taking movement arguments and returning a player action result.
    - Sets the foundation for implementing various player movements with different strategies or behaviors.

---
+++ src/durak/OpenCards.Durak/Movements/IPlayerMovement.cs
@@ -0,0 +1,8 @@
+﻿using OpenCards.Durak.Movements.Results;
+
+namespace OpenCards.Durak.Movements;
+
+public interface IPlayerMovement
+{
+    ValueTask<IPlayerActionResult> Make(MovementArguments arguments);
+}
