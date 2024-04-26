48461d8558277f2f7c7269e71f66d602157d0634
sipasi <tanksasha77@gmail.com>
2024-04-26 13:33:38 +0300

    feat: implement PlayerMovement Abstract Class
    - Added the PlayerMovement abstract class within the OpenCards.Durak.Movements namespace.
    - Implements the IPlayerMovement interface, providing a base for various player movements.
    - Defines a Make method to asynchronously execute a player movement using provided movement arguments.
    - Utilizes an IMovementValidator instance to validate the result of the player movement.
    - Handles validation failures by returning appropriate PlayerWronged results, considering the context of the movement.
    - Encapsulates the specific movement logic within derived classes by defining the MakeMove method as abstract.

---
+++ src/durak/OpenCards.Durak/Movements/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using OpenCards.Durak.Movements.Results;
+
+namespace OpenCards.Durak.Movements;
+
+public abstract class PlayerMovement(IMovementValidator validator) : IPlayerMovement
+{
+    public async ValueTask<IPlayerActionResult> Make(MovementArguments arguments)
+    {
+        IPlayerActionResult result = await MakeMove(arguments);
+
+        if (validator.Validate(result, arguments))
+        {
+            return result;
+        }
+
+        return result switch
+        {
+            PlayerWronged wronged => wronged,
+            PlayerMoved moved => new PlayerWronged(moved.Card),
+            _ => new PlayerWronged(),
+        };
+    }
+
+    protected abstract ValueTask<IPlayerActionResult> MakeMove(MovementArguments arguments);
+}
