6caef1477c8d04a3e29b3752ea458dbf8f7bbfb1
sipasi <tanksasha77@gmail.com>
2024-04-26 13:34:33 +0300

    feat: Implement BotMovement Abstract Class and SimpleBotMove Class
    - Introduced the BotMovement abstract class within the OpenCards.Durak.Movements namespace, serving as a base for bot-specific player movements.
    - Inherits from PlayerMovement and leverages its functionality for movement execution and validation.
    - Implements the MakeMove method to define the behavior of bot movements based on different action types and board states.
    - Provides abstract methods for specific bot movement strategies, such as FirstAttack, NextAttack, Defend, and Toss.
    - Implemented the SimpleBotMove class as a concrete implementation of bot movement strategy, utilizing simple logic for attacking, defending, and tossing cards.
    - Encapsulates the logic of bot movement strategies within the SimpleBotMove class, adhering to the BotMovement contract.
    - Employs a suitably named class and method to reflect the simplicity and clarity of the implemented bot movement strategy.

---
+++ src/durak/OpenCards.Durak/Movements/BotMovement.cs
@@ -0,0 +1,22 @@
+﻿using OpenCards.Durak.Movements.Results;
+
+namespace OpenCards.Durak.Movements;
+
+public abstract class BotMovement(IMovementValidator validator) : PlayerMovement(validator)
+{
+    protected sealed override ValueTask<IPlayerActionResult> MakeMove(MovementArguments arguments) => arguments switch
+    {
+        { ActionType: PlayerActionType.Attack, Board.IsEmpty: true } => FirstAttack(arguments).AsValueTask(),
+        { ActionType: PlayerActionType.Attack, Board.IsEmpty: false } => NextAttack(arguments).AsValueTask(),
+        { ActionType: PlayerActionType.Defend } => Defend(arguments).AsValueTask(),
+        { ActionType: PlayerActionType.Toss } => Toss(arguments).AsValueTask(),
+        _ => throw new NotImplementedException($"Can't handle [{arguments.ActionType}] type")
+    };
+
+    public abstract IPlayerActionResult FirstAttack(MovementArguments arguments);
+    public abstract IPlayerActionResult NextAttack(MovementArguments arguments);
+
+    public abstract IPlayerActionResult Defend(MovementArguments arguments);
+
+    public abstract IPlayerActionResult Toss(MovementArguments arguments);
+}
---
+++ src/durak/OpenCards.Durak/Movements/SimpleBotMove.cs
@@ -0,0 +1,59 @@
+﻿using OpenCards.Cards.SuitsRanks;
+using OpenCards.Durak.Movements.Results;
+
+namespace OpenCards.Durak.Movements;
+
+public sealed class SimpleBotMove(IMovementValidator validator) : BotMovement(validator)
+{
+    public override IPlayerActionResult Defend(MovementArguments arguments)
+    {
+        var (player, deck, board) = (arguments.Current, arguments.Deck, arguments.Board);
+
+        SuitRankCard trump = deck.Trump;
+        SuitRankCard last = board.Attacks.Last();
+
+        SuitRankCard? selected = player.Hand
+            .MinRankWhere(card => card.EqualSuit(last) && card.Rank > last.Rank);
+
+        if (selected is null && last.EqualSuit(trump) == false)
+        {
+            selected = player.Hand.MinRankBy(trump.Suit);
+        }
+
+        return selected is null
+            ? new PlayerPassed()
+            : new PlayerMoved(selected);
+    }
+
+    public override IPlayerActionResult FirstAttack(MovementArguments arguments)
+    {
+        var (player, deck) = (arguments.Current, arguments.Deck);
+
+        SuitRankCard card =
+            arguments.Current.Hand.MinRankWhere(card => card.Suit != deck.Trump.Suit) ??
+            arguments.Current.Hand.MinRank()!;
+
+        return new PlayerMoved(card);
+    }
+
+    public override IPlayerActionResult NextAttack(MovementArguments arguments)
+    {
+        var (player, board) = (arguments.Current, arguments.Board);
+
+        SuitRankCard? selected = default;
+
+        foreach (var card in player.Hand)
+        {
+            if (board.Any(card.EqualRank))
+            {
+                selected = card;
+            }
+        }
+
+        return selected is null
+            ? new PlayerPassed()
+            : new PlayerMoved(selected);
+    }
+
+    public override IPlayerActionResult Toss(MovementArguments arguments) => NextAttack(arguments);
+}
