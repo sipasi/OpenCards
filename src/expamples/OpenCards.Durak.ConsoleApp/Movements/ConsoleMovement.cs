480527bc771a1f817269d5ddd4f8c43e21d928b5
sipasi <tanksasha77@gmail.com>
2024-04-27 11:47:37 +0300

    feat: Implement console-based player movement for Durak game
    This commit introduces the ConsoleMovement class responsible for creating player movement instances based on the specified move type. Additionally, it implements the LocalMove class, which represents a player movement strategy for local (console-based) players.
    
    - ConsoleMovement class implements the IPlayerMovementFactory interface to provide player movement instances based on the specified move type.
    - LocalMove class extends the PlayerMovement abstract class and represents a player movement strategy for local (console-based) players.
    - The MakeMove method in the LocalMove class allows players to make moves via console input, handling various input scenarios such as passing and playing cards.
    - The GetMove method in the LocalMove class processes console input to determine the player's move, including passing, playing specific cards, or indicating an invalid move.
    
    This commit enhances the Durak console application by providing a mechanism for local players to interact with the game via console input, allowing for manual moves during gameplay.

---
+++ src/expamples/OpenCards.Durak.ConsoleApp/Movements/ConsoleMovement.cs
@@ -0,0 +1,14 @@
+﻿using OpenCards.Durak.Movements;
+using OpenCards.Durak.Players;
+
+namespace OpenCards.Durak.ConsoleApp;
+
+public class ConsoleMovement : IPlayerMovementFactory
+{
+    public IPlayerMovement Get(MoveType move) => move switch
+    {
+        MoveType.Bot => new SimpleBotMove(MovementValidator.Default),
+        MoveType.Local => new LocalMove(MovementValidator.Default),
+        _ => throw new NotImplementedException($"{move} has not implemented")
+    };
+}
---
+++ src/expamples/OpenCards.Durak.ConsoleApp/Movements/LocalMove.cs
@@ -0,0 +1,44 @@
+﻿using OpenCards.Cards.SuitsRanks;
+using OpenCards.Collections.Boards;
+using OpenCards.Collections.Hands;
+using OpenCards.Durak.Movements;
+using OpenCards.Durak.Movements.Results;
+using OpenCards.Durak.Players;
+
+namespace OpenCards.Durak.ConsoleApp;
+
+public sealed class LocalMove(IMovementValidator validator) : PlayerMovement(validator)
+{
+    protected override ValueTask<IPlayerActionResult> MakeMove(MovementArguments arguments)
+    {
+        var (player, _, board, _) = arguments;
+
+        string? line = Console.ReadLine() ?? throw new NullReferenceException();
+
+        IPlayerActionResult move = GetMove(player, line, board);
+
+        return move.AsValueTask();
+    }
+
+    public static IPlayerActionResult GetMove(IReadonlyPlayer current, string? input, IReadonlyBoard<SuitRankCard> board)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return new PlayerWronged();
+        }
+
+        if (input is "p")
+        {
+            return board.IsEmpty
+                ? new PlayerWronged()
+                : new PlayerPassed();
+        }
+
+        if (int.TryParse(input, out var result) && current.Hand.InRange(result))
+        {
+            return new PlayerMoved(current.Hand[result]);
+        }
+
+        return new PlayerWronged();
+    }
+}
