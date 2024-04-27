69eb958610d4868f11eb62270e6202272ba70beb
sipasi <tanksasha77@gmail.com>
2024-04-27 11:51:52 +0300

    feat: add main program entry point for Durak console application
    This commit introduces the Program class, which serves as the main entry point for the Durak console application. The Program class configures the console encoding to support Unicode characters and initializes the game state machine using the DurakConsoleBuilder. It then enters a loop to continuously execute the game state machine, allowing players to interact with the game and observe events in real-time.
    
    - Main method sets the console input and output encoding to support Unicode characters, ensuring proper display of text and symbols.
    - CreateGame method utilizes the DurakConsoleBuilder to create a default game setup with predefined event observers registered to handle game events.
    - PlayStateMachine method continuously executes the game state machine, allowing the game to progress and handle player interactions and game events.
    
    This commit lays the foundation for the Durak console application, providing a structured approach to game initialization and execution, as well as event handling and observation.

---
+++ src/expamples/OpenCards.Durak.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+using OpenCards.Durak.StateMachines;
+using OpenCards.States;
+
+namespace OpenCards.Durak.ConsoleApp;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.InputEncoding = Encoding.GetEncoding(1200);
+        Console.OutputEncoding = Encoding.UTF8;
+
+        IStateMachine game = CreateGame();
+
+        await PlayStateMachine(game);
+    }
+
+    public static IStateMachine CreateGame() => DurakConsoleBuilder
+        .Default()
+
+        .RegisterEvent(new GameStartObserver())
+        .RegisterEvent(new GameEndObserver())
+        .RegisterEvent(new RoundStartObserver())
+        .RegisterEvent(new RoundExecutingObserver())
+        .RegisterEvent(new RoundExecutedObserver())
+
+        .Build();
+
+
+    public static async ValueTask PlayStateMachine(IStateMachine game)
+    {
+        while (true)
+        {
+            await game.Execute();
+        }
+    }
+}
