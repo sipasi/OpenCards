ab6ba3d415cba37dfee089a421bdf18a1580ce0f
sipasi <tanksasha77@gmail.com>
2024-04-27 11:51:22 +0300

    feat: add console-based Durak game builder with user input
    This commit introduces the DurakConsoleBuilder static class responsible for initializing and configuring the DurakBuilder instance based on user input in the console application. The DurakConsoleBuilder provides methods to interactively prompt the user for deck size and player configuration, allowing for customizable game setups.
    
    - Init method prompts the user to specify the deck size and player configuration interactively, returning a DurakBuilder instance configured accordingly.
    - Default method provides a default game setup with a medium deck size and one local player ("You") and one bot player ("Bot").
    - AskDeckSize method prompts the user to select the deck size from available options using console input.
    - AskPlayers method prompts the user to specify the number of players and configures player names and move types accordingly.
    - Ask method is a generic method used internally to prompt the user for selection from a list of options, validating the input and returning the selected value.
    
    This commit enhances the Durak console application by allowing users to customize game settings interactively, providing a more engaging and dynamic gameplay experience.

---
+++ src/expamples/OpenCards.Durak.ConsoleApp/DurakConsoleBuilder.cs
@@ -0,0 +1,65 @@
+﻿using OpenCards.Durak.Game.Builders;
+using OpenCards.Durak.Game.Settings;
+using OpenCards.Durak.Players;
+
+namespace OpenCards.Durak.ConsoleApp;
+
+internal static class DurakConsoleBuilder
+{
+    public static DurakBuilder Init() => new()
+    {
+        DeckSize = AskDeckSize(),
+        Players = AskPlayers(),
+        MovementFactory = new ConsoleMovement()
+    };
+    public static DurakBuilder Default() => new()
+    {
+        DeckSize = DeckSize.Medium,
+        Players = [("You", MoveType.Local), ("Bot", MoveType.Bot)],
+        MovementFactory = new ConsoleMovement()
+    };
+
+
+    private static DeckSize AskDeckSize() => Ask<DeckSize>(Enum.GetValues<DeckSize>(), "Enter deck size: ", static size => $"{size}({(int)size})");
+    private static (string, MoveType)[] AskPlayers()
+    {
+        int count = Ask<int>([2, 3, 4], "Player count: ", static size => size.ToString());
+
+        var players = new (string, MoveType)[count];
+
+        players[0] = ("You", MoveType.Local);
+
+        for (int i = 1; i < count; i++)
+        {
+            players[i] = ($"Bot: {i}", MoveType.Bot);
+        }
+
+        return players;
+    }
+
+    private static T Ask<T>(ReadOnlySpan<T> values, string title, Func<T, string> formatter)
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            int index = 0;
+
+            foreach (var item in values)
+            {
+                Console.WriteLine($"{index++}: {formatter.Invoke(item)}");
+            }
+
+            Console.WriteLine();
+
+            Console.Write(title);
+
+            string? line = Console.ReadLine();
+
+            if (int.TryParse(line, out int result) && result >= 0 && result < values.Length)
+            {
+                return values[result];
+            }
+        }
+    }
+}
