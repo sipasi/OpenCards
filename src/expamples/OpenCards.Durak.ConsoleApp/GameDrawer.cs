cfc1380d8ee4a240817cbd624a029948ae6eac47
sipasi <tanksasha77@gmail.com>
2024-04-27 11:48:27 +0300

    feat: Implement game state and input visualization in Durak console application
    This commit introduces the GameDrawer static class responsible for visualizing the current game state and input prompts in the Durak console application. The GameDrawer class provides methods to draw the deck, board, player hands, and input prompts, enhancing the user experience during gameplay.
    
    - Draw method in the GameDrawer class visualizes the current game state by drawing the deck, board, and player hands.
    - DrawInputState method displays an input prompt based on the current player's move type, indicating whether the player should make a move or wait for the bot or server.
    - DrawDeck, DrawBoard, and DrawPlayers methods draw the deck, board, and player hands, respectively, using console output.
    - DrawLineColor and DrawColor methods allow drawing text with specified foreground colors, enhancing visual clarity and highlighting important information.
    - Clear method clears the console screen and resets the console color, ensuring a clean display for each game state update.
    
    This commit enhances the Durak console application by providing visual feedback to the player, improving the overall gameplay experience.

---
+++ src/expamples/OpenCards.Durak.ConsoleApp/GameDrawer.cs
@@ -0,0 +1,163 @@
+﻿using OpenCards.Cards.SuitsRanks;
+using OpenCards.Collections.Boards;
+using OpenCards.Collections.Hands;
+using OpenCards.Collections.Players;
+using OpenCards.Durak.Collections.Decks;
+using OpenCards.Durak.Game;
+using OpenCards.Durak.Players;
+
+namespace OpenCards.Durak.ConsoleApp;
+
+public static class GameDrawer
+{
+    public static void Draw(IReadonlyGameState state)
+    {
+        Clear();
+
+        var (deck, board) = (state.Deck, state.Board);
+
+        DrawDeck(deck);
+
+        Console.WriteLine();
+
+        DrawPlayers(state);
+
+        Console.WriteLine();
+
+        DrawBoard(board);
+    }
+
+    public static void DrawInputState(IReadonlyPlayerQueue<IReadonlyPlayer> queue)
+    {
+        IReadonlyPlayer current = queue.Current;
+
+        string message = current.MoveType switch
+        {
+            MoveType.Local => $"Enter from 0 to {current.Hand.Count - 1} or press p to pass: ",
+            MoveType.Bot => "Wait, Bot is Thinking",
+            MoveType.Server => "Wait for server side",
+            _ => throw new NotImplementedException(),
+        };
+
+        Console.WriteLine();
+
+        DrawColor(ConsoleColor.Blue, message);
+    }
+
+    private static void DrawDeck(IReadonlyDurakDeck deck)
+    {
+        Console.WriteLine($"{deck.Trump} - {deck.Count}");
+    }
+    private static void DrawBoard(IReadonlyBoard<SuitRankCard> board)
+    {
+        for (int i = 0; i < board.All.Count; i++)
+        {
+            SuitRankCard data = board.All[i];
+
+            Console.WriteLine(data);
+        }
+    }
+
+    private static void DrawPlayers(IReadonlyGameState state)
+    {
+        foreach (var player in state.Storage.Active)
+        {
+            var (id, name, hand) = (player.Id, player.Name, player.Hand);
+
+            IReadonlyPlayerQueue<IReadonlyPlayer> queue = state.Queue;
+
+            bool current = id == queue.Current.Id;
+
+            string star = current ? "*" : " ";
+            string defender = id == queue.Defender.Id ? "🛡" : " ";
+
+            ConsoleColor color = current ? ConsoleColor.DarkGreen : ConsoleColor.DarkGray;
+
+            DrawLineColor(color, $"{star}{defender} {name} ({hand.Count}): ");
+
+            DrawPlayerHand(state, player, current, hide: false);
+        }
+    }
+    private static void DrawPlayerHand(IReadonlyGameState state, IReadonlyPlayer player, bool current, bool hide)
+    {
+        var (name, moveType, hand) = (player.Name, player.MoveType, player.Hand);
+
+        foreach (var (card, text) in HandAsStrings(hand, secure: hide))
+        {
+            if (current is false)
+            {
+                Console.WriteLine(text);
+
+                continue;
+            }
+
+            if (state.Queue.IsDefenderQueue)
+            {
+                SuitRankCard last = state.Board.Attacks.Last();
+
+                if (card.CanBeat(last, state.Deck.Trump))
+                {
+                    DrawLineColor(ConsoleColor.Yellow, text);
+                }
+                else
+                {
+                    Console.WriteLine(text);
+                }
+
+                continue;
+            }
+
+            if (state.Board.IsEmpty is false)
+            {
+                if (state.Board.All.Any(card.EqualRank))
+                {
+                    DrawLineColor(ConsoleColor.Yellow, text);
+                }
+                else
+                {
+                    Console.WriteLine(text);
+                }
+            }
+            else
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+
+    private static IEnumerable<(SuitRankCard, string)> HandAsStrings(IReadOnlyHand<SuitRankCard> hand, bool secure = true)
+    {
+        var pattern = hand.Select((card, index) => secure switch
+        {
+            true => (card, "*"),
+            false => (card, $"    ({index}) {card}")
+        });
+
+        // Spade = U+2660, Heart = U+2665, Diamond = U+2666, Heart = U+2663
+
+        return pattern;
+    }
+
+    public static void DrawLineColor(ConsoleColor color, string text)
+    {
+        Console.ForegroundColor = color;
+
+        Console.WriteLine(text);
+
+        Console.ResetColor();
+    }
+    public static void DrawColor(ConsoleColor color, string text)
+    {
+        Console.ForegroundColor = color;
+
+        Console.Write(text);
+
+        Console.ResetColor();
+    }
+
+    public static void Clear()
+    {
+        Console.Clear();
+        Console.ResetColor();
+    }
+}
